65fad84b52e7b42d332a088a26ecd3f855d85dbd
Andrew Laski <andrew@lascii.com>
2016-04-20 16:12:43 -0400

    Add custom exception classes
    This adds a base exception for the project and then some specific
    exceptions for objects. The msg_fmt paradigm is copied from other
    OpenStack projects.

--- enamel/objects/task_item.py
+++ enamel/objects/task_item.py
@@ -17,6 +17,7 @@ from oslo_versionedobjects import fields
 from enamel.db import models as db_models
 from enamel.db import utils as db_utils
 from enamel.objects import base
+from enamel.objects import exception as obj_exception
 
 
 @ovo_base.VersionedObjectRegistry.register
@@ -48,7 +49,9 @@ class TaskItem(base.EnamelTimestampObject, base.EnamelObject):
         db_task_item = session.query(db_models.TaskItem).filter_by(
                                                             uuid=uuid).first()
 
-        # TODO(alaski): raise exception if db_task_item not found
+        if not db_task_item:
+            raise obj_exception.TaskItemNotFound(uuid=uuid)
+
         return db_task_item
 
     @classmethod
